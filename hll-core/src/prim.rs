b00307d6a55467daf0512ca44ea0786c5e4516cd
nagi <sekidoharune@gmail.com>
2025-09-18 23:28:33 +0900

    add prim_to_cons_unchecked

--- hll-core/src/prim.rs
+++ hll-core/src/prim.rs
@@ -1,6 +1,6 @@
 use crate::types::{Cons, Prim};
 
-pub fn cons_to_prim_unchecked(cons: Cons, gamma:f64) -> Prim {
+pub fn cons_to_prim_unchecked(cons: Cons, gamma: f64) -> Prim {
     let mut prim = Prim::default();
 
     prim.u = cons.rhou / cons.rho;
@@ -9,4 +9,15 @@ pub fn cons_to_prim_unchecked(cons: Cons, gamma:f64) -> Prim {
     prim.a = (gamma * prim.p / cons.rho).sqrt();
 
     prim
+}
+
+pub fn prim_to_cons_unchecked(prim: Prim, gamma: f64) -> Cons {
+    let mut cons = Cons::default();
+
+    cons.rho = gamma * prim.p / (prim.a * prim.a);
+    cons.rhou = cons.rho * prim.u;
+    cons.rhov = cons.rho * prim.v;
+    cons.e = prim.p / (gamma - 1.0) + cons.rho * (prim.u * prim.u + prim.v * prim.v) / 2.0;
+    
+    cons
 }

b905f03a6cf214c6ae331a67b5b5666997d241be
nagi <sekidoharune@gmail.com>
2025-09-18 22:42:08 +0900

    add prim.rs

---
+++ hll-core/src/prim.rs
@@ -0,0 +1,12 @@
+use crate::types::{Cons, Prim};
+
+pub fn cons_to_prim_unchecked(cons: Cons, gamma:f64) -> Prim {
+    let mut prim = Prim::default();
+
+    prim.u = cons.rhou / cons.rho;
+    prim.v = cons.rhov / cons.rho;
+    prim.p = (gamma - 1.0) * (cons.e - cons.rho * (prim.u * prim.u + prim.v * prim.v) / 2.0);
+    prim.a = (gamma * prim.p / cons.rho).sqrt();
+
+    prim
+}
